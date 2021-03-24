482fcdd2cec614f249b57457e65f437ecd5e0f29
Nikita <6025342+nikita-kun@users.noreply.github.com>
2021-03-24 22:53:54 +0100

    ipv6 fix

--- autorun.sh
+++ autorun.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 sudo sed -i "s/# deb/deb/" /etc/apt/sources.list
+sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
 sudo apt update
 sudo apt-get --yes install php
 sudo chmod -R +x *.php
